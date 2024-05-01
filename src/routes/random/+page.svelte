2f42f6102ca356b8263fa19630b06ece0b3f9b8a
Intzaaa <huangyinhaow@outlook.com>
2024-05-01 17:41:55 +0800

    Refactor list layout and input fields in +page.svelte

--- src/routes/random/+page.svelte
+++ src/routes/random/+page.svelte
@@ -132,7 +132,7 @@ Christopher Taylor
 		@apply flex h-full w-full flex-col items-center justify-start overflow-y-scroll md:flex-row md:justify-center md:overflow-visible md:pb-0;
 	}
 	.list {
-		@apply flex h-fit max-h-[50%] min-h-28 w-full shrink-0 grow-0 flex-row flex-wrap content-start items-stretch justify-start overflow-y-scroll p-2 md:!h-full md:!max-h-full md:!w-[calc(1024px-300px)] md:!content-stretch;
+		@apply flex h-fit max-h-[50%] min-h-12 w-full shrink-0 grow-0 flex-row flex-wrap content-start items-stretch justify-start overflow-y-scroll p-2 md:!h-full md:!max-h-full md:!w-[calc(1024px-300px)] md:!content-stretch;
 	}
 	.item {
 		@apply m-2 h-fit min-w-36 grow text-ellipsis p-3 text-lg outline outline-[1.5px] md:h-auto;
