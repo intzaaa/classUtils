3f70a5dc84c888cbc0d5bdbf5392fa3f87e8be3a
Intzaaa <huangyinhaow@outlook.com>
2024-05-01 17:26:20 +0800

    Refactor list layout and input fields in +page.svelte

--- src/routes/random/+page.svelte
+++ src/routes/random/+page.svelte
@@ -54,103 +54,99 @@
 }}
 />
 
-<main>
-	<div class="content">
-		{#key randomList}
-			<mdui-card class="list card">
-				{#if randomList.length === 0}
-					<div class="flex h-full w-full flex-col items-center justify-center text-center">
-						Empty
-					</div>
-				{:else}
-					{#each randomList as item, index}
-						<!-- svelte-ignore a11y_click_events_have_key_events -->
-						<!-- svelte-ignore a11y_no_static_element_interactions -->
-						<mdui-chip selectable class="item"
-							>{item}<span
-								slot="selected-icon"
-								style="font-family: 'Material Symbols Outlined Variable'; font-size: 0.9em"
-								>person_check</span
-							></mdui-chip
-						>
-					{/each}
-				{/if}
-			</mdui-card>
-		{/key}
-		<mdui-card class="control card">
-			<mdui-tooltip content="Enter the number of people to be randomly selected" placement="bottom">
-				<mdui-text-field
-					label={`${list.length > 0 ? `0 < Number ≤ ${list.length}` : 'Number'}`}
-					type="number"
-					class="number"
-					placeholder={number}
-					oninput={(e: InputEvent) => {
+<main class="content">
+	{#key randomList}
+		<mdui-card class="list card">
+			{#if randomList.length === 0}
+				<div class="flex h-full w-full flex-col items-center justify-center text-center">Empty</div>
+			{:else}
+				{#each randomList as item, index}
+					<!-- svelte-ignore a11y_click_events_have_key_events -->
+					<!-- svelte-ignore a11y_no_static_element_interactions -->
+					<mdui-chip selectable class="item"
+						>{item}<span
+							slot="selected-icon"
+							style="font-family: 'Material Symbols Outlined Variable'; font-size: 0.75em"
+							>person_check</span
+						></mdui-chip
+					>
+				{/each}
+			{/if}
+		</mdui-card>
+	{/key}
+	<mdui-card class="control card">
+		<mdui-tooltip content="Enter the number of people to be randomly selected" placement="bottom">
+			<mdui-text-field
+				label={`${list.length > 0 ? `0 < Number ≤ ${list.length}` : 'Number'}`}
+				type="number"
+				class="number"
+				placeholder={number}
+				oninput={(e: InputEvent) => {
 						const element = e.target;
 						if (element instanceof HTMLInputElement) {
 							number = parseInt(element.value === '' ? '1' : element.value);
 						}
 					}}
-				></mdui-text-field>
-			</mdui-tooltip>
-			<mdui-tooltip content="Enter the list you want to randomly select from" placement="bottom">
-				<mdui-text-field
-					min-rows="4"
-					rows={listElementHeight === 0 ? 6 : Math.floor((listElementHeight - (24 + 8)) / 24)}
-					label="List"
-					placeholder={`Michael Lee
+			></mdui-text-field>
+		</mdui-tooltip>
+		<mdui-tooltip content="Enter the list you want to randomly select from" placement="bottom">
+			<mdui-text-field
+				min-rows="4"
+				rows={listElementHeight === 0 ? 6 : Math.floor((listElementHeight - (24 + 8)) / 24)}
+				label="List"
+				placeholder={`Michael Lee
 Emily Chen
 David Martinez
 Jessica Nguyen
 Christopher Taylor
 ...`}
-					type="text"
-					oninput={(e: InputEvent) => {
+				type="text"
+				oninput={(e: InputEvent) => {
             const element = e.target;
             if (element instanceof HTMLTextAreaElement) {
               rawList = element.value;
             }
           }}
-					class="input"
-					bind:this={listElement}
-				></mdui-text-field>
-			</mdui-tooltip>
-		</mdui-card>
-	</div>
-	<!-- svelte-ignore a11y_click_events_have_key_events -->
-	<!-- svelte-ignore a11y_no_static_element_interactions -->
-	<mdui-fab
-		icon="start"
-		extended
-		disabled={list.length === 0 || number > list.length || number < 1}
-		onclick={() => {
-			randomList = calculateRandomList(list, number);
-		}}
-	>
-		Execute
-	</mdui-fab>
+				class="input"
+				bind:this={listElement}
+			></mdui-text-field>
+		</mdui-tooltip>
+	</mdui-card>
 </main>
+
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<mdui-fab
+	icon="start"
+	extended
+	disabled={list.length === 0 || number > list.length || number < 1}
+	onclick={() => {
+		randomList = calculateRandomList(list, number);
+	}}
+>
+	Execute
+</mdui-fab>
 
 <style lang="postcss">
 	main {
-		@apply flex h-full w-full flex-col items-center justify-start;
-	}
-	.content {
-		@apply relative flex w-full flex-col md:h-full md:max-w-[1024px] md:flex-row;
+		@apply flex h-full w-full flex-col items-center justify-start overflow-y-scroll md:h-full md:max-w-[1024px] md:flex-row;
 	}
 	.list {
-		@apply flex h-full w-full flex-row flex-wrap content-center items-stretch justify-between overflow-y-scroll p-2 md:!h-full md:content-stretch;
+		@apply flex h-fit max-h-[50%] min-h-28 w-full shrink-0 grow-0 flex-row flex-wrap content-start items-stretch justify-start overflow-y-scroll p-2 md:!h-full md:content-stretch;
 	}
 	.item {
-		@apply m-2 h-fit min-w-36 grow text-ellipsis p-3 text-center text-lg outline outline-[1.5px] md:h-auto;
+		@apply m-2 h-fit min-w-36 grow text-ellipsis p-3 text-lg outline outline-[1.5px] md:h-auto;
 	}
 	.item::part(button) {
-		@apply w-full overflow-y-hidden overflow-x-scroll;
+		justify-content: center;
+		justify-content: safe center;
+		@apply flex w-full flex-row items-center overflow-y-hidden overflow-x-scroll;
 	}
 	.card {
-		@apply mb-2 mt-2 flex h-fit  w-full p-4 md:mb-0 md:ml-4 md:mr-4 md:mt-0;
+		@apply mb-2 mt-2 flex h-fit w-full p-4 md:mb-0 md:ml-4 md:mr-4 md:mt-0;
 	}
 	.control {
-		@apply h-96 w-full flex-col overflow-y-hidden md:h-full md:max-w-[300px];
+		@apply h-96 w-full shrink-0 grow-0 flex-col overflow-y-hidden md:h-full md:max-w-[300px];
 	}
 	.number {
 		@apply w-full;
